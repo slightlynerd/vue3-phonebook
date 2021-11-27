11e05fcd52cbc01cc8522296733cbeee50276ca6
Kelvin Gobo <gobokel@gmail.com>
2021-11-27 22:27:26 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,163 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="phonebook">
+    <div class="header row">
+      <img alt="logo" src="./assets/logo.jpeg" height="90">
+      <h1>Phonebook</h1>
+    </div>
+    <form @submit.prevent="addToPhonebook">
+      <div class="row">
+        <input v-model="firstName" type="text" placeholder="First name">
+        <input v-model="lastName" type="text" placeholder="Last name">
+      </div>
+      <div class="row">
+        <input v-model="email" type="email" placeholder="Email" required>
+        <input v-model="phone" type="text" placeholder="Phone" required>
+      </div>
+      <button :disabled="!isFormValid" type="submit" class="btn-add">
+        {{ editMode ? 'Edit' : 'Add' }} Contact
+      </button>
+    </form>
+
+    <h3>Contacts</h3>
+    <input
+      v-model="searchTerm"
+      type="text"
+      placeholder="Search contacts"
+      class="input-search w-100"
+    >
+    <p v-if="!filteredContacts.length">No contacts yet! Use the form above to add a new contact.</p>
+    <ul>
+      <li
+        v-for="(item, index) in filteredContacts"
+        :key="item.id"
+        class="contact"
+      >
+        <div class="row">
+          <div class="contact-initials">
+            {{ item.firstName ? item.firstName[0] : '' }}
+            {{ item.lastName ? item.lastName[0] : '' }}
+          </div>
+          <div class="row contact-details">
+            <div class="contact-info">
+              <span class="contact-name">
+                {{ item.firstName }} {{ item.lastName }}
+              </span>
+              <br>
+              <span class="contact-phone">
+                {{ item.phone }},
+              </span>
+              <span class="contact-email">
+                {{ item.email }}
+              </span>
+            </div>
+            <div class="contact-actions">
+              <button @click="editNumber(index)">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: #3f51b5;transform: ;msFilter:;"><path d="M19.045 7.401c.378-.378.586-.88.586-1.414s-.208-1.036-.586-1.414l-1.586-1.586c-.378-.378-.88-.586-1.414-.586s-1.036.208-1.413.585L4 13.585V18h4.413L19.045 7.401zm-3-3 1.587 1.585-1.59 1.584-1.586-1.585 1.589-1.584zM6 16v-1.585l7.04-7.018 1.586 1.586L7.587 16H6zm-2 4h16v2H4z"></path></svg>
+              </button>
+              <button @click="deleteNumber(index)">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: #3f51b5;transform: ;msFilter:;"><path d="M6 7H5v13a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V7H6zm10.618-3L15 2H9L7.382 4H3v2h18V4z"></path></svg>
+              </button>
+            </div>
+          </div>
+        </div>
+        <hr>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import {
+  defineComponent,
+  ref,
+  computed,
+  watchEffect,
+  watch,
+} from 'vue';
+import { useStore } from 'vuex';
 
-export default {
+export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld,
+
+  setup() {
+    // data
+    const firstName = ref('');
+    const lastName = ref('');
+    const email = ref('');
+    const phone = ref('');
+    const searchTerm = ref('');
+    const isFormValid = ref(false);
+
+    // store
+    const store = useStore();
+
+    const filteredContacts = computed(() => store.state.filteredContacts);
+    const editMode = computed(() => store.state.editMode);
+
+    function addToPhonebook() {
+      store.commit('addContact', {
+        firstName: firstName.value,
+        lastName: lastName.value,
+        email: email.value,
+        phone: phone.value,
+      });
+      firstName.value = '';
+      lastName.value = '';
+      email.value = '';
+      phone.value = '';
+    }
+
+    function editNumber(index) {
+      const item = filteredContacts.value[index];
+      firstName.value = item.firstName;
+      lastName.value = item.lastName;
+      email.value = item.email;
+      phone.value = item.phone;
+      store.commit('editContact', index);
+    }
+
+    function deleteNumber(index) {
+      store.commit('deleteContact', index);
+    }
+
+    function validateEmail(e) {
+      const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(String(e).toLowerCase());
+    }
+
+    watch(searchTerm, (newValue) => {
+      store.commit('searchContact', newValue);
+    });
+
+    watchEffect(() => {
+      if (
+        email.value && phone.value
+        && (firstName.value || lastName.value)
+        && validateEmail(email.value)
+      ) {
+        isFormValid.value = true;
+      } else {
+        isFormValid.value = false;
+      }
+    });
+
+    return {
+      addToPhonebook,
+      firstName,
+      lastName,
+      email,
+      phone,
+      deleteNumber,
+      editNumber,
+      filteredContacts,
+      editMode,
+      isFormValid,
+      searchTerm,
+    };
   },
-};
+});
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import '@/assets/styles/main.scss';
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,54 @@
 import { createStore } from 'vuex';
+import VuexPersistence from 'vuex-persist';
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage,
+});
 
 export default createStore({
   state: {
+    contacts: [],
+    filteredContacts: [],
+    editMode: false,
+    editIndex: false,
   },
   mutations: {
+    addContact(state, contact) {
+      if (state.editMode) {
+        state.contacts[state.editIndex].firstName = contact.firstName;
+        state.contacts[state.editIndex].lastName = contact.lastName;
+        state.contacts[state.editIndex].email = contact.email;
+        state.contacts[state.editIndex].phone = contact.phone;
+        state.editMode = false;
+        state.editIndex = null;
+        return;
+      }
+      state.contacts.push({
+        id: Date.now(),
+        firstName: contact.firstName,
+        lastName: contact.lastName,
+        email: contact.email,
+        phone: contact.phone,
+      });
+      state.filteredContacts = [...state.contacts];
+    },
+    editContact(state, index) {
+      state.editMode = true;
+      state.editIndex = index;
+      state.filteredContacts = [...state.contacts];
+    },
+    deleteContact(state, index) {
+      state.contacts.splice(index, 1);
+      state.filteredContacts = [...state.contacts];
+    },
+    searchContact(state, search) {
+      state.filteredContacts = state.contacts.filter(
+        (contact) => contact.firstName.toLowerCase().includes(search.toLowerCase())
+          || contact.lastName.toLowerCase().includes(search.toLowerCase())
+          || contact.email.toLowerCase().includes(search.toLowerCase())
+          || contact.phone.toLowerCase().includes(search.toLowerCase()),
+      );
+    },
   },
-  actions: {
-  },
-  modules: {
-  },
+  plugins: [vuexLocal.plugin],
 });
